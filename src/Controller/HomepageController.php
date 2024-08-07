8a17cb693ade5b9fbe966b9917381c3502380b62
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-07 11:35:43 +0200

    First traduction added

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomepageController extends AbstractController
 {
-    #[Route('/', name: 'app_homepage')]
+    #[Route('/{_locale}', name: 'app_homepage', requirements: ['_locale' => 'en|fr'])]
     public function index(): Response
     {
         return $this->render('homepage/index.html.twig');

5014142a1aab46befa4bf5a28309a12c1d232a8d
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-08 03:39:44 +0200

    Form informations according the language

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -1,18 +1,20 @@
 <?php 
+
 namespace App\Controller;
 
+use App\Form\RegisterUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
-    #[Route([
-        'en' => '/register',
-        'fr' => '/inscription'
-    ], name: 'app_register')]
+    #[Route(['en' => '/register', 'fr' => '/inscription'], name: 'app_register')]
     public function index(): Response
     {
-        return $this->render('register/register.html.twig');
+        $form = $this->createForm(RegisterUserType::class);
+        return $this->render('register/register.html.twig', [
+            'formRegister' => $form->createView()
+        ]);
     }
 }

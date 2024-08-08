f7eb9a1e5b52962253d4fc04fc91e11889e91835
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-08 03:03:19 +0200

    Translatioin of Regsiter page

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -1,14 +1,16 @@
-<?php
-
+<?php 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
-    #[Route('/inscription', name: 'app_register')]
+    #[Route([
+        'en' => '/register',
+        'fr' => '/inscription'
+    ], name: 'app_register')]
     public function index(): Response
     {
         return $this->render('register/register.html.twig');

ce8c131e363afcc617e93e8dda9655fa1a034956
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-10 03:12:20 +0200

    Register Controller updated

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -1,9 +1,12 @@
 <?php 
 
+// src/Controller/RegisterController.php
+
 namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterUserType;
+use App\Service\MailerService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,18 +16,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class RegisterController extends AbstractController
 {
     #[Route(['en' => '/register', 'fr' => '/inscription'], name: 'app_register')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, MailerService $mailerService): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterUserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Créer un token de confirmation (à sauvegarder dans l'utilisateur)
+            $user->setConfirmationToken(rtrim(strtr(base64_encode(random_bytes(32)), '+/', '-_'), '='));
+
             $entityManager->persist($user);
             $entityManager->flush();
 
-            $this->redirectToRoute('app_homepage');
+            // Envoyer l'email de validation
+            $mailerService->sendValidationEmail($user);
+
+            // Ajouter un message flash
+            $this->addFlash('success', 'Un email de confirmation a été envoyé. Veuillez vérifier votre boîte de réception.');
+
+            return $this->redirectToRoute('app_homepage');
         }
+
         return $this->render('register/register.html.twig', [
             'formRegister' => $form->createView()
         ]);

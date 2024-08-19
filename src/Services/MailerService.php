19f2645afa4c1f2050fe47e684c920fc5a92034e
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-19 23:59:54 +0200

    Mail Service

--- src/Services/MailerService.php
+++ src/Services/MailerService.php
@@ -5,41 +5,49 @@ namespace App\Service;
 use SendGrid;
 use SendGrid\Mail\Mail;
 use App\Entity\User;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Twig\Environment;
 
 class MailerService
 {
     private $sendGrid;
+    private $twig;
+    private $router;
 
-    public function __construct(string $sendGridApiKey)
+    public function __construct(string $sendGridApiKey, Environment $twig, UrlGeneratorInterface $router)
     {
         $this->sendGrid = new SendGrid($sendGridApiKey);
+        $this->twig = $twig;
+        $this->router = $router;
     }
 
     public function sendValidationEmail(User $user): void
     {
         $email = new Mail();
-        
-        // Assurez-vous que l'adresse "From" est autorisée par SendGrid
-        $email->setFrom("emarh.harry.code@gmail.com", "Emarh Perfume");
 
-        $email->setSubject("Please Confirm Your Email");
+        $email->setFrom("emarhdureal@gmail.com", "Emarh Perfume");
+
+        // Générer l'URL de confirmation avec le token
+        $confirmationUrl = $this->router->generate('app_confirm_email', ['token' => $user->getConfirmationToken()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        // Charger le template Twig
+        $emailContent = $this->twig->render('emails/confirmation_email.html.twig', [
+            'user' => $user,
+            'confirmationUrl' => $confirmationUrl
+        ]);
+
+        // Récupérer l'objet du mail et le contenu HTML du corps du mail
+        $email->setSubject($this->twig->render('emails/confirmation_email.html.twig', ['user' => $user])->getSubject());
         $email->addTo($user->getEmail(), $user->getFirstName() . ' ' . $user->getLastName());
-        $email->addContent(
-            "text/html",
-            "<p>Thank you for registering! Please confirm your email by clicking the following link:</p>" .
-            "<p><a href='http://yourdomain.com/confirm/{$user->getConfirmationToken()}'>Confirm Email</a></p>"
-        );
+        $email->addContent("text/html", $emailContent);
 
         try {
             $response = $this->sendGrid->send($email);
-            // Ajout d'un dump pour vérifier le statut de la réponse
-            dump($response);
 
             if ($response->statusCode() >= 400) {
                 throw new \Exception('Failed to send email: ' . $response->body());
             }
         } catch (\Exception $e) {
-            // Log the error or handle it as needed
             error_log('Email sending failed: ' . $e->getMessage());
         }
     }

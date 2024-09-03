e0e2adf26731d97f283fdf27e9acf33a80cfc05e
Harry EMARH <emarh.harry.code@gmail.com>
2024-09-04 00:12:11 +0200

    Configuration for User Entity

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -15,14 +16,20 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Utilisateurs')
+            ->setEntityLabelInPlural('Utilisateurs');
+    }
+
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('firstName')->setLabel('Prénom'),
+            TextField::new('lastName')->setLabel('Nom'),
+            TextField::new('email')->setLabel('Email')->onlyOnIndex(),
         ];
     }
-    */
 }
